9bd708a49495c74966e67ccdcf3e6f71c005c005
Luis <luisdiazvenero@gmail.com>
2018-03-27 16:35:53 -0500

    logica de datalist para ofrecer opciones

--- autocomplete/main.js
+++ autocomplete/main.js
@@ -1,4 +1,35 @@
-class AUtocomplete{}
+class Autocomplete{
+  constructor(input_selector, base_url){
+    this.input = document.querySelector(input_selector);
+    this.url = base_url;
+    this.buildDataList();
+
+  }
+
+  buildDataList(){
+    this.dataList = document.createElement("datalist");
+    this.dataList.id = "datalist-autocomplete"; // todo: el string deberia generarse random
+    document.querySelector("body").appendChild(this.dataList);
+    this.input.setAttribute("list", "datalist-autocomplete");
+  }
+
+  search(){
+    Search.get(this.url+"harry")
+      .then(results => this.build(results));
+  }
+
+  build(response){
+    this.dataList.innerHTML = "";
+    response.items.forEach(item => {
+      let optionEl = document.createElement("option");
+      optionEl.value = item.volumeInfo.title;
+      if(item.volumeInfo.subtitle)
+        optionEl.innerHTML = item.volumeInfo.title;
+
+        this.dataList.appendChild(optionEl);
+    });
+  }
+}
 
 class Search{
   static get(url){
@@ -20,8 +51,9 @@ class Search{
 
 (function () {
   const GoogleBooksApiURL = "https://www.googleapis.com/books/v1/volumes?q=";
-  Search.get(GoogleBooksApiURL+"harry")
-    .then(results => console.log(results));
+  let autocomplete = new Autocomplete("#searcher", GoogleBooksApiURL);
+  autocomplete.search();
+
 }) ();
 
-Search.get("").then(data=>{ })
+//Search.get("").then(data=>{ })
